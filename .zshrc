f47d8eb97b2b2f283d046f77b90fb65539e69985
Kaylee <kaylee.ulep@gmail.com>
2025-08-26 10:07:47 -0400

    fix(ui): resolved Tailwind and DaisyUI issues by updating dependencies

---
+++ .zshrc
@@ -0,0 +1,10 @@
+# NVM setup
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+# Add local node_modules binaries of the current folder
+export PATH="$PWD/node_modules/.bin:$PATH"
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./skysong.css"; //replaces import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./AuthContext";
 // import reportWebVitals from './reportWebVitals';
